f419912be005858d3f670bc24cd5803fcd6d76a0
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-15 21:13:12 -0300

    feat: Criar arquivo principal de esculta do app

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { config } from "dotenv";
+import app from "./app/app";
+
+config();
+
+const portApplication: string | undefined = process.env.PORT;
+
+app.listen(portApplication, () => {
+    console.log(`Application running on the port ${portApplication}`);
+})
