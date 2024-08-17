5940e3a8f5ed33dacff41d760f2fa578a5256a5f
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-16 23:43:06 -0300

    feat: Criar controller para user e adicionar funções de create
    Issue: #1

---
+++ src/controllers/User/index.ts
@@ -0,0 +1,84 @@
+import { Request, Response } from "express";
+import { createHashPassword } from "../../utils/bcrypt";
+import User from "../../models/User";
+import Permission from "../../models/Permission";
+
+interface IUserCreateDTO {
+    name: string,
+    email: string,
+    password: string,
+    cpf: string
+}
+
+const create = async (req: Request, res: Response) => {
+    try {
+        const userToCreate: IUserCreateDTO = req.body;
+
+        userToCreate.password = await createHashPassword(userToCreate.password);
+
+        const userPermission = await Permission.findFirst({ where: { role: 'user' } });
+
+        if (!userPermission) {
+            return res.status(400).send({ message: "User role not found" });
+        }
+
+        const userResponse = await User.create({
+            data: {
+                ...userToCreate,
+                avatar: '',
+                permission_id: userPermission.id
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                password: true
+            }
+        })
+
+        res.status(200).send(userResponse)
+    } catch (error: any) {
+        res.status(500).send({
+            message: "Error on try create User"
+        })
+    }
+}
+
+const createAdminUser = async (req: Request, res: Response) => {
+    try {
+        const userToCreate: IUserCreateDTO = req.body;
+
+        userToCreate.password = await createHashPassword(userToCreate.password);
+
+        const userPermission = await Permission.findFirst({ where: { role: 'admin' } });
+
+        if (!userPermission) {
+            return res.status(400).send({ message: "User role not found" });
+        }
+
+        const userResponse = await User.create({
+            data: {
+                ...userToCreate,
+                avatar: '',
+                permission_id: userPermission.id
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                password: true
+            }
+        })
+
+        res.status(200).send(userResponse)
+    } catch (error: any) {
+        res.status(500).send({
+            message: "Error on try create User"
+        })
+    }
+}
+
+export default {
+    create,
+    createAdminUser
+}
