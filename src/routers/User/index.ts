fed9811a620c2e90480c28f428bb944881c7a569
João Darwin <joaodarwin.ip22@gmail.com>
2024-08-17 10:07:15 -0300

    feat: Remover dos endpoints de user, signup de usuário
    Issue: #5

--- src/routers/User/index.ts
+++ src/routers/User/index.ts
@@ -3,7 +3,6 @@ import UserController from "../../controllers/User"
 
 const userRouter = Router();
 
-userRouter.post("/save", UserController.create);
 userRouter.post("/saveAdminUser", UserController.createAdminUser); // This endpoint will have permission middleware, to check if who's creating is other admin
 userRouter.get("/", UserController.findAll); // Add auth middleware and add permission middleware
 userRouter.get("/:id", UserController.findById); // Add auth middleware and add permission middleware
